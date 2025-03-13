418daf09550fa2dded1048f432f48f253f3a7497
OllieBL <oliver.black4@education.nsw.gov.au>
2025-03-13 14:42:53 +1100

    Completed the integration task
    completed the integration task

---
+++ Integration/calculator.py
@@ -0,0 +1,7 @@
+from math_operations import add_numbers
+
+a = int(input('Please input a whole number: '))
+
+b = int(input('Please input another whole number: '))
+
+print(add_numbers(a, b))
---
+++ Integration/math_operations.py
@@ -0,0 +1,3 @@
+def add_numbers(a, b):
+    answer = a + b
+    return answer
